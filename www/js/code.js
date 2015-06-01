ef536327bfb512abc2948c1813851db3ceae5c6b
Forge <artem0100@ukr.net>
2015-06-01 15:54:05 +0300

    2.5.6
    Заполнил Data.js

--- www/js/code.js
+++ www/js/code.js
@@ -1,1 +1,85 @@
-$(function(){var t=window.localStorage.getItem("surname"),o=window.localStorage.getItem("tid");redirect=function(){var n=location.pathname.indexOf("register.html")+1?!0:!1;null==t&&null==o&&0==n?location="register.html":null!=t&&null!=o&&1==n&&(location="index.html")},footer_information=function(){null!=t&&null!=o&&$('div[data-role="footer"] h3').html(t+" ~ "+o)},clear_not_full_question=function(){for(var t=!1,o=0;o<data.length;o++)null==window.localStorage.getItem("r"+o)&&(t=!0);if(t)for(var o=0;o<=data.length;o++)window.localStorage.removeItem("r"+o)},init=function(){redirect(),footer_information(),clear_not_full_question()},init()});
+// Функция находит число, которое повторяется большее количество раз в массиве arr
+find_more_reapet = function(answ0, answ1, answ2) {
+	var i,j,k,l;
+	for(var i=5;i<=12;i++)
+	{
+		var r = window.localStorage.getItem('r' + i);
+		if(r == 0) i++;
+		else if(r == 1) j++;
+		else if(r == 2) k++;
+	}
+
+	if((i > j) && (i > k))
+		l = 0;
+	else if((j > i) && (j > k))
+		l = 1;
+	else
+		l = 2;
+
+	if(l == 0)
+		return answ0;
+	else if(l == 1)
+		return answ1;
+	else
+		return answ2;
+}
+
+$(function() {
+		// Достаем с localstorage Фамилию
+		var surname = window.localStorage.getItem("surname"),
+		// Достаем с localstorage Табельный номер
+			tid = window.localStorage.getItem("tid");
+
+	// Функция проверки и перенаправлений
+	redirect = function() {
+		// Проверка на то, что мы находимся на странице Регистрации
+		var check_page_register = location.pathname.indexOf('register.html') + 1 ? true : false;
+		// Проверка на отсутствие куков Фамилии и Табельного номера, а также что сейчас не находимся на странице Регистрации
+		if((surname == null) && (tid == null) && check_page_register == false) {
+			// Перенаправление на страницу Регистрации
+			location = 'register.html';
+		// Проверка на то, что куки Фамилии и Табельного номера у нас не пустые,
+		// а также что мы находимся на странице Регистрации
+		} else if((surname != null) && (tid != null) && check_page_register == true) {
+			// Перенаправление на страницу тестов
+			location = 'index.html';
+		}
+
+		// alert(location.pathname.indexOf('register.html') + 1 ? true : false);
+		// alert(location.pathname == '/register.html');
+	}
+
+	// Функция добавляющая Фамилию и Табельный номер в подвал сайта
+	footer_information = function() {
+		// Проверка на то, что куки Фамилии и Табельного номера не пусты
+		if((surname != null) && (tid != null))
+			// Присвоение h3 тегу в подвале нашего сайта значения Фамилии ~ Табельного номера
+			$('div[data-role="footer"] h3').html(surname + ' ~ ' + tid);
+	}
+
+	// Функция Очищение не законченых тестов
+	clear_not_full_question = function() {
+		var a = false;
+		for(var i=0;i<data.length;i++)
+			if(window.localStorage.getItem('r' + i) == null)
+				a = true;
+
+		if(a)
+			for(var i=0;i<=data.length;i++)
+				// Удаление текущего localstorage
+				window.localStorage.removeItem('r'+i);
+
+	}
+
+	init = function() {
+		// Функция проверки и перенаправлений
+		redirect();
+		// Функция добавляющая Фамилию и Табельный номер в подвал сайта
+		footer_information();
+		// Функция Очищение не законченых тестов
+		clear_not_full_question();
+	}
+
+	init();
+
+});
--- www/js/result.js
+++ www/js/result.js
@@ -1,1 +1,52 @@
-$(function(){var t="#result";result=function(){var l="",e="";if(null==window.localStorage.getItem("r0"))return void $(t).html('<h3 align="center">Результаты теста отсутствуют</h3>');for(var i=0;i<data.length;i++){var a=window.localStorage.getItem("r"+i);l+="<li><h3>"+data[i][0]+"</h3><p>"+data[i][3][a]+"</p>"}e+='<h3>Результат</h3><ol data-role="listview">'+l+"</ol>",$(t).html(e)},init=function(){result(),$('[data-role="listview"]').listview().listview("refresh")},init()});
+$(function() {
+	// id главного блока, где размещаються все ответы
+	var block_id = '#result';
+
+	// Функция генерации списка ответов
+	result = function() {
+			// Список вопросов и соответствующей рекомендации на её
+		var list = '',
+			// Блок в который помещается список
+			block = '';
+
+		// Проверка первого варианта ответа на пустоту
+		if(window.localStorage.getItem("r0") == null){
+			// Сообщение которое выводится в случае отсутствия ответов в куках
+			$(block_id).html('<h3 align="center">Результаты теста отсутствуют</h3>');
+			// Прерывание дальнейшего выполнения этой функции
+			return;
+		}
+
+		// Проходимся по всем записям из массива data и выводим их в сгенерированые блоки
+		for(var i=0;i<data.length;i++) {
+			var res = window.localStorage.getItem('r' + i);
+
+			// Списки ответов
+			list += '<li>'+
+						'<h3>'+ data[i][0] +'</h3>'+
+						'<p>'+ data[i][3][res] +'</p>'
+					'</li>';  
+		}
+
+		// Общий блок в который помещается списки ответов
+	    block += 	'<h3>Результат</h3>'+
+		    		'<ol data-role="listview">'+
+					    list+
+					'</ol>';
+
+		// Вывод сгенерированого блока в тег с id result
+		$(block_id).html(block);
+	}
+
+	// Функция инициализации
+	init = function() {
+		// Вызов функции генерации списка ответов
+		result();
+		// Переобразование и добавление стилей к списку
+		$('[data-role="listview"]').listview().listview('refresh');
+		// $(block_id).trigger("create");
+	}
+
+	// Вызов функции инициализации
+	init();
+});
